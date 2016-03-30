c3e65054a73a4013ebd25f334c21122a060a7ec6
Thijs Elferink <thijs.elferink@topicus.nl>
2016-03-30 11:55:38 +0200

    ff en sync laten werken met srcBranch en branchMenu naar common

--- _git-common.sh
+++ _git-common.sh
@@ -5,4 +5,44 @@ handle_error() {
     exit 1
 }
 
+function branchMenu() {
+	if [ -z $srcBranch ]; then
+		exit 0;
+	fi
+
+	function printBranchMenu() {
+		echo "Selecteer de source-branch:"
+		echo "m) master (volgende release)"
+		echo "v) valuepack (volgende valuepack)"
+		echo "a) acceptatie (acceptatie patch)"
+		echo "p) patch (productie patch)"
+	}
+	printBranchMenu
+	while :
+	do
+	    echo
+		echo "Selecteer een optie [m,v,a,p]?"
+		read CHOICE
+	    case "$CHOICE" in
+	        "m")
+	            srcBranch="master"
+	            break
+	            ;;
+	        "v")
+	            srcBranch="valuepack"
+	            break
+	            ;;
+	        "a")
+	            srcBranch="acceptatie"
+	            break
+	            ;;
+	        "p")
+	            srcBranch="patch"
+	            break
+	            ;;
+	    esac
+	    echo "Ongeldige optie!"
+	done
+}
+
 trap 'handle_error $0 $LINENO $?' ERR
